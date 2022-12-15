01d00bd3f94cc6c65e1892668b33a27af0068044
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-14 16:09:13 -0800

    finished_debugging

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,29 +12,33 @@ const initdb = async () =>
     },
   });
 
-// Method that accepts some content and adds it to the database
+/// Method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Put to the database');
-  const jateDb = await openDB('jate, 1');
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+
   const tx = jateDb.transaction('jate', 'readwrite');
+
   const store = tx.objectStore('jate');
+
   const request = store.put({ id: 1, value: content });
+
   const result = await request;
-  console.log('Data saved onto the database', result);
+
+  console.log('Data saved onto the database', result.value);
 };
 
 // Method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-  const jateDb = await openDB('jate, 1');
+  const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('data saved to the jateDB', result.value);
   // if result cannot be accessed, it returns undefined instead of throwing an error.
-  return result?.value;
+  return result.value;
 };
-
 
 initdb();
